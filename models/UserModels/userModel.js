697bf76de91a7166950761367f385c2c97cd2cba
codewithkevin <oseikelvin709@gmail.com>
2023-02-16 14:23:49 +0000

    Created more Schema for userModel

--- models/UserModels/userModel.js
+++ models/UserModels/userModel.js
@@ -2,9 +2,41 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
   {
+    account: {
+      type: String,
+      required: [true, "Please add a account"],
+    },
+    workplace: {
+      type: String,
+      required: [true, "Please add a workplace"],
+    },
     name: {
       type: String,
       required: [true, "Please add a name"],
+    },
+    date_of_birht: {
+      type: String,
+      required: [true, "Please add a date_of_birht"],
+    },
+    phone_number: {
+      type: String,
+      required: [true, "Please add a phone_number"],
+    },
+    country: {
+      type: String,
+      required: [true, "Please add a country"],
+    },
+    username: {
+      type: String,
+      unique: true,
+    },
+    age: {
+      type: String,
+      required: [true, "Please add a age"],
+    },
+    interests: {
+      type: String,
+      required: [true, "Please add a interests"],
     },
     email: {
       type: String,
@@ -14,10 +46,6 @@ const userSchema = mongoose.Schema(
     password: {
       type: String,
       required: [true, "Please add a password"],
-    },
-    username: {
-      type: String,
-      unique: true,
     },
   },
   { timeStamps: true }
